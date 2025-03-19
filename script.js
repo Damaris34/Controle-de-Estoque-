59c4d039a0fadbc51621852b80f4f2955da219d1
Damaris34 <oliveiradamares211@gmail.com>
2025-03-19 16:54:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ function gerarExcel() {
     const responsavel = document.getElementById('responsavel').value;
     const material = document.getElementById('material').value;
     const dataRelatorio = document.getElementById('data-relatorio').value;
+    const quantidade = document.getElementById('quantidade').value;
+    const observacoes = document.getElementById('observacoes').value;
 
     const dados = [
-        { "Data do Registro": dataRegistro, "Nome do Responsável": responsavel, "Nome do Material": material, "Data para Relatório": dataRelatorio }
+        {
+            "Data do Registro": dataRegistro,
+            "Nome do Responsável": responsavel,
+            "Nome do Material": material,
+            "Data para Relatório": dataRelatorio,
+            "Quantidade em Estoque": quantidade,
+            "Observações": observacoes
+        }
     ];
 
     const worksheet = XLSX.utils.json_to_sheet(dados);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Relatório');
 
-    XLSX.writeFile(workbook, 'Relatório_de_Materiais.xlsx');
+    XLSX.writeFile(workbook, 'Relatório_de_Estoque.xlsx');
 }
