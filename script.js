7890d7e482008f64d28f3d6a03012ac432768220
Damaris34 <oliveiradamares211@gmail.com>
2025-03-27 16:49:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function adicionarLinha() {
+    const dataRegistro = document.getElementById('dataRegistro').value;
     const table = document.getElementById('estoqueTable').getElementsByTagName('tbody')[0];
     const newRow = table.insertRow();
 
     const cells = [
-        `<td><input type="date"></td>`,
+        `<td>${dataRegistro}</td>`,
         `<td><input type="text"></td>`,
         `<td><input type="number"></td>`,
         `<td><select><option value="Sim">Sim</option><option value="Não">Não</option></select></td>`,
         `<td><input type="number"></td>`,
+        `<td><input type="text"></td>`,
         `<td><button onclick="removerLinha(this)">Remover</button></td>`
     ];
 
@@ -20,4 +22,3 @@ function removerLinha(button) {
     const row = button.parentNode.parentNode;
     row.parentNode.removeChild(row);
 }
-
