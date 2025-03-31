45451a7cb13544410f02b453bae144db2f9dcebc
Damaris34 <oliveiradamares211@gmail.com>
2025-03-31 14:41:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function registrar() {
     // Lógica para registrar os dados do formulário
     alert('Dados registrados com sucesso!');
 }
-
-function gerarPDF() {
-    // Lógica para gerar o PDF
-    alert('PDF gerado com sucesso!');
-}
