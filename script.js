3c43352ed53a280958c93c12d3afff1f1e176a84
Damaris34 <oliveiradamares211@gmail.com>
2025-04-01 16:17:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 document.getElementById('gerarPDF').addEventListener('click', () => {
     const { jsPDF } = window.jspdf;
 
